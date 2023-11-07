41862e8bc7b527e5132c095fb2985b0851a6ae31
Patrik Cesnek <46940455+PatrikCesnek@users.noreply.github.com>
2023-11-07 13:32:18 +0100

    Add HomeView

---
+++ PlaceholderJSON/PlaceholderJSON/PlaceholderJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderJSONApp.swift
+//  PlaceholderJSON
+//
+//  Created by Patrik Cesnek on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlaceholderJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
